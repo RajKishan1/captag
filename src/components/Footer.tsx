7ef3e97ba1078b2b7a7d66f6e2d57f983e9b3fa6
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-07-16 16:52:06 +0530

    fix:ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <hr />
-      <p className="text-xs text-center mt-4">Made with ❤️ by Raj Kishan</p>
+      <p className="text-xs text-center my-4">Made with ❤️ by Raj Kishan</p>
     </div>
   );
 };
--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -1,32 +1,36 @@
 import React from "react";
+interface prop {
+  title: string;
+  text: string;
+  buttonText: string;
+}
+export const FeatureDiv = ({ text, title, buttonText }: prop) => {
+  return (
+    <div className=" w-4/5 p-2 rounded-xl flex flex-col items-center gap-2  bg-neutral-800">
+      <h2 className="my-1.5 text-center text-2xl ">{title}</h2>
+      <span className="flex flex-col items-center gap-3 bg-neutral-900 rounded-lg px-1 py-3">
+        <p className="text-center">{text}</p>
+        <a href="#" className="px-3  py-2 border rounded-lg">
+          {buttonText}
+        </a>
+      </span>
+    </div>
+  );
+};
 const Features = () => {
   return (
     <div className="w-full  flex flex-col gap-4">
       <div className="flex flex-col items-center justify-between gap-5">
-        <div className=" w-4/5 py-3 rounded-xl border flex flex-col gap-3 border-neutral-50 bg-gradient-to-bl from-neutral-800 to-neutral-900">
-          <h2 className="my-5 text-center text-2xl ">Image to Caption</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            delectus, ipsum provident sed ex eveniet quasi impedit! Quod,
-            doloribus facere?{" "}
-          </p>
-
-          <a href="#" className="px-3 py-2 border rounded-lg">
-            Try Now
-          </a>
-        </div>
-        <div className=" w-4/5 py-3 rounded-xl border flex flex-col gap-3 border-neutral-50 bg-gradient-to-bl from-neutral-800 to-neutral-900">
-          <h2 className="my-5 text-center text-2xl ">Image to Caption</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            delectus, ipsum provident sed ex eveniet quasi impedit! Quod,
-            doloribus facere?{" "}
-          </p>
-
-          <a href="#" className="px-3 py-2 border rounded-lg">
-            Try Now
-          </a>
-        </div>
+        <FeatureDiv
+          text=" aofsvnpiaefughcwpfegwhruitgvuhvw sslfjkvnaeliruigaghnb aer;oicahneit "
+          title="erwrw"
+          buttonText="sdfs"
+        />
+        <FeatureDiv
+          text=" aofsvnpiaefughcwpfegwhruitgvuhvw sslfjkvnaeliruigaghnb aer;oicahneit aeira"
+          title="erwrw"
+          buttonText="sdfs"
+        />
       </div>
     </div>
   );
