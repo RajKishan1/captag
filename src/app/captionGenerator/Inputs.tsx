a21fd896cc479fd1768096efb49f87d7a11807ac
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-07-17 16:49:25 +0530

    fix:ui

---
+++ src/app/captionGenerator/Inputs.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Inputs = () => {
+  return (
+    <div>
+      <div className="w-full flex items-center  justify-evenly gap-2 px-2  py-4">
+        <div className="text-white text-sm bg-[#202020] px-3 py-2 rounded-lg ">
+          <label htmlFor="">Platform:</label>
+          <select name="" id="">
+            <option>Facebook</option>
+            <option>Instagram</option>
+          </select>
+        </div>
+        <div className="text-white text-sm bg-[#202020] px-3 py-2 rounded-lg">
+          <label htmlFor="">Mood:</label>
+          <select name="" id="">
+            <option>Happy😊</option>
+            <option>Sad🙁</option>
+          </select>
+        </div>
+      </div>{" "}
+      <div className="w-[90%] mx-auto my-3 rounded-lg  px-2 py-4 border bg-[#202020]">
+        <input
+          type="text"
+          className="w-full h-full"
+          placeholder="Custom Keywords (Optional)"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Inputs;
--- src/app/captionGenerator/page.tsx
+++ src/app/captionGenerator/page.tsx
@@ -1,28 +1,14 @@
 import React from "react";
 import { FileUpload } from "./FileUpload";
 import NavigationBar from "@/components/homepage/NavigationBar";
+import Inputs from "./Inputs";
 
 const page = () => {
   return (
     <div className="">
       <NavigationBar />
       <FileUpload />
-      <div className="w-full flex items-center  justify-around gap-2 px-2 bg-red-600 py-4">
-        <div className="text-white bg-red-50">
-          <label htmlFor="">Mood</label>
-          <select name="" id="">
-            <option>Facebook</option>
-            <option>Instagram</option>
-          </select>
-        </div>
-        <div className="text-white">
-          <label htmlFor="">Mood</label>
-          <select name="" id="">
-            <option>Facebook</option>
-            <option>Instagram</option>
-          </select>
-        </div>
-      </div>
+      <Inputs />
     </div>
   );
 };
